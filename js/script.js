afccef984acd440fcfbf70087d6ca1f85a052b9c
adprm <aditiyaprmn00@gmail.com>
2020-08-16 14:03:41 +0700

    active class menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ $.getJSON('data/menu.json', function (data) {
         $('#daftar-menu').append('<div class="col-md-4"><div class= "card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5>Rp. ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div >')
     });
 });
+
+$('.nav-link').on('click', function () {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+});
