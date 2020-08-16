2c98d7e3392d34d7d854df15eb1e8435a2be3fa8
adprm <aditiyaprmn00@gmail.com>
2020-08-16 13:26:17 +0700

    get data json

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $.getJSON('data/burger.json', function (data) {
-    console.log(data);
-})
+    let menu = data.menu;
+    // looping objek
+    $.each(menu, function (index, data) {
+        $('#daftar-menu').append('<div class="col-md-4"><div class= "card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5>Rp. ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div >')
+    });
+});
